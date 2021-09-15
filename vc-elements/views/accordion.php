dd26595fc9bc2247df065147b1cf1c6dfc9e7e0c
Matt McNamee <matt@mcnamee.co>
2021-09-15 15:50:49 +1000

    PHPCS Lint vs elements

---
+++ vc-elements/views/accordion.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * View for Accordion Element
+ *
+ * @package salient-child
+ */
+
+$acc_title = $atts['accordion_title'] ?? false;
+
+?>
+<div class="pvtl-accordion">
+	<h2 class="acc-title"><?php echo esc_attr( $acc_title ); ?></h2>
+	<div class="toggles accordion" data-starting="default" data-style="default">
+		<?php echo do_shortcode( $content ); ?>
+	</div>
+</div>
